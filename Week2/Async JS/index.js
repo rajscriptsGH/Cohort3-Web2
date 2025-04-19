df923fb4ddf40793950f307c4cff62093ea69013
rajyadav <vector36razz@gmail.com>
2025-04-19 22:52:27 +0530

    week2 folder added

---
+++ Week2/Async JS/index.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+// const content = fs.readFileSync("a.txt", "utf-8");
+// console.log(content);
+
+// const content2 = fs.readFileSync("b.txt", "utf-8");
+// console.log(content2);
+
+fs.readFile("b.txt", 'utf-8', function(err, content){
+    console.log(content);
+    
+})
