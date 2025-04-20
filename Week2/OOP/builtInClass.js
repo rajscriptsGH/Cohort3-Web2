55d121fd6a768104d068d37cbff19682b3083e61
rajyadav <vector36razz@gmail.com>
2025-04-20 14:19:20 +0530

    builtin class added

---
+++ Week2/OOP/builtInClass.js
@@ -0,0 +1,11 @@
+const date = new Date();
+// console.log(date);
+console.log(date.getMonth());
+
+
+const map = new Map()
+map.set('name', 'Razz')
+map.set('age', 21)
+map.set('year', '3rd')
+
+console.log(map.get('year'));
