0a6a15bcbd03a5c5fbba4fb15b7956998c862609
rajyadav <vector36razz@gmail.com>
2025-04-21 23:56:44 +0530

    callback assignment  file added

---
+++ Week2/Promise/callbackHell.js
@@ -0,0 +1,20 @@
+/* 
+Write code that
+1. logs hi after 1 second
+2. logs hello 3 seconds after step 1
+3. logs hello there 5 seconds after step 2
+*/
+
+//Without Promise
+
+setTimeout(() => {
+    console.log("hi");
+    setTimeout(() => {
+        console.log("Hello");
+        setTimeout(() => {
+            console.log("hello there");
+        }, 5000)
+    }, 3000)
+}, 1000)
+
+
--- Week2/Promise/index.js
+++ Week2/Promise/index.js
@@ -1,8 +1,31 @@
-console.log("Start");
+// console.log("Start");
 
-//normal setTimeOut
-setTimeout(() => {
-    console.log("1");
-}, 1000)
+// //normal setTimeOut
+// setTimeout(() => {
+//     console.log("1");
+// }, 1000)
 
-console.log("2");
+// console.log("2");
+
+// function setTimeOutPromisified(ms) {
+//     let promise = new Promise((resolve) => {
+//         setTimeout(resolve, ms);
+//     })
+//     return promise;
+// }
+
+// function callback() {
+//     console.log("it is callback");
+// }
+
+// setTimeOutPromisified(1000).then(callback)
+
+function waitfor3S(resolve) {
+    setTimeout(resolve, 3000)
+}
+
+function main() {
+    console.log("Main is called");
+}
+
+waitfor3S(main)
