89ad42967014051e74f68d835ef16de9aad28c9b
rajyadav <vector36razz@gmail.com>
2025-04-22 14:35:06 +0530

    Challenge of promise done

---
+++ Week2/Promise/Challenge1.js
@@ -0,0 +1,46 @@
+/*
+
+💻 Challenge: Login → Fetch Dashboard → Show Data
+🎯 Goal:
+Simulate a login system that:
+
+Checks if the user is logged in ✅
+
+If logged in, fetches dashboard data 📊
+
+Displays it OR handles any error properly ❌
+
+*/
+
+function userDashboard() {
+  const user = {
+    name: "Razz",
+    handle: "rajscripts100x",
+    isLogged: true,
+  };
+  return user;
+}
+
+let user = userDashboard();
+// console.log(user.name);
+
+function isUserLoggedIn() {
+  return new Promise((res, rej) => {
+    if (user.isLogged == false) {
+      res(user);
+    } else {
+      rej("not logged");
+    }
+  });
+}
+
+let promise = isUserLoggedIn();
+console.log(promise);
+
+promise
+  .then((data) => {
+    console.log(user);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
