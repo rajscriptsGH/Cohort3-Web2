bd128ea65176c53cc7b81ad34a9de41fc3649f48
rajyadav63 <vector36razz@gmail.com>
2025-05-05 00:27:40 +0530

    server updated

--- Week-05 Headers, Query, Middleware & Cors/D1 Header, Query & Express/03_HTTP Server/index.js
+++ Week-05 Headers, Query, Middleware & Cors/D1 Header, Query & Express/03_HTTP Server/index.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 
 
-app.get('/add', (req, res) => {
+app.get('/add', (req, res) => {                                  //here we need to pass as localhost:port/sum?a=2&b=3...
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
     if (isNaN(a) || isNaN(b)) {
@@ -15,9 +15,9 @@ app.get('/add', (req, res) => {
 
     res.json({ ans })
 })
-app.get('/subtract', (req, res) => {
-    const a = req.query.a;
-    const b = req.query.b;
+app.get('/subtract/:a/:b', (req, res) => {                               //to pass localhost:port/suntract/2/3...
+    const a = req.params.a;                                              //change query to params
+    const b = req.params.b;
     if (isNaN(a) || isNaN(b)) {
         return res.status(400).json({ error: "Invalid query" })
     }
