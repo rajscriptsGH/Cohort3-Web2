5c641258652042c63cc3fbee07cd2c02f06a05e5
rajyadav63 <vector36razz@gmail.com>
2025-05-05 22:51:12 +0530

    middleware done

--- Week-05 Headers, Query, Middleware & Cors/D2 Middleware and Cors/index.js
+++ Week-05 Headers, Query, Middleware & Cors/D2 Middleware and Cors/index.js
@@ -5,7 +5,13 @@ const port = 3000;
 
 let requestCount = 0;
 
+/*
 //middleware
+1. it may or may not change the request object
+2. it will 
+    -either stop the request right there 
+    -or forward the request to the real route handler
+  */
 function requestIncrease(req, res, next) {
     requestCount = requestCount + 1;
     req.name = "razzzzzzzzz";
@@ -17,16 +23,15 @@ function realSumHandler(req, res) {
     // requestIncrease()                                 //this function is added as middleware
     const a = parseInt(req.query.a);
     const b = parseInt(req.query.b);
-    console.log(req.name);
 
+    console.log(req.name);
 
     const ans = a + b;
     res.json({ ans })
 }
 
+// app.use(realSumHandler)                               //global middleware
 app.get('/add', requestIncrease, realSumHandler);
-
-
 
 
 app.listen(port, () => {
