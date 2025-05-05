0625fe9e65f77b30c9f44c3cf87f36aa9701b95e
rajyadav63 <vector36razz@gmail.com>
2025-05-06 02:52:31 +0530

    cors added

--- Week-05 Headers, Query, Middleware & Cors/D2 Middleware and Cors/CORS/index.js
+++ Week-05 Headers, Query, Middleware & Cors/D2 Middleware and Cors/CORS/index.js
@@ -6,10 +6,29 @@ Assignment-
 */
 
 import express from 'express'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 const app = express();
 const port = 3000;
 
+
+// These 2 lines are needed to get __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Enable CORS
+app.use(cors({
+    origin: '*',           // or use specific origin: 'http://localhost:5000'
+}));
+
 app.use(express.json())
+
+// Serve HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/sum', (req, res) => {
     const a = parseInt(req.body.a);
