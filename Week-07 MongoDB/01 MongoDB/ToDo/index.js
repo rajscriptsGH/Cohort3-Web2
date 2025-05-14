e6fbd0d9aed799cb4073c8d7fef74618daef21e5
rajyadav63 <vector36razz@gmail.com>
2025-05-14 17:23:23 +0530

    MongoDb added

--- Week-07 MongoDB/01 MongoDB/ToDo/index.js
+++ Week-07 MongoDB/01 MongoDB/ToDo/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 const port = 3000;
+
+app.use(express.json())
 
 app.post('/signup', (req, res) => {
 
