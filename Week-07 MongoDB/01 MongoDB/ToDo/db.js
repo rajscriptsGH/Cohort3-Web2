4beb682f71377447661ebac40da0cb61599756ea
rajyadav63 <vector36razz@gmail.com>
2025-05-15 01:19:34 +0530

    signup and signin done

---
+++ Week-07 MongoDB/01 MongoDB/ToDo/db.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+//Schema of my db
+const User = new Schema({
+    username: String,
+    password: String,
+    age: Number,
+    name: String
+});
+const ToDo = new Schema({
+    desc: String,
+    done: Boolean,
+    userId: ObjectId
+});
+
+//let insert data in User using Model and // Prevent OverwriteModelError 
+const UserModel = mongoose.models.users || mongoose.model('users', User);
+const TodoModel = mongoose.models.todos || mongoose.model('todos', ToDo);
+
+//to user these model in other js file, we need to export this file
+export {
+    UserModel, TodoModel
+};
--- Week-07 MongoDB/01 MongoDB/ToDo/index.js
+++ Week-07 MongoDB/01 MongoDB/ToDo/index.js
@@ -1,27 +1,64 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express, { json } from 'express'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
+import { UserModel, TodoModel } from './db.js'
 
+mongoose.connect("mongodb+srv://rajscripts100x:Fd34oovuyS5NIYsJ@cluster0.mqrp3kn.mongodb.net/todo-razz-6363")
 const app = express()
 const port = 3000;
 
+const JWT_SECRET = "jdahbjncwihfk"
+
 app.use(express.json())
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
 
+    const username = req.body.username
+    const password = req.body.password
+    const name = req.body.name
+    const age = req.body.age
+
+    await UserModel.create({
+        username: username,
+        password: password,
+        age: age,
+        name: name
+    })
+    res.json({
+        username: username,
+        msg: "U are signed up"
+    })
 })
-app.post('/login', (req, res) => {
+app.post('/signin', async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
 
+    const user = await UserModel.findOne({
+        username: username,
+        password: password
+    })
+
+    if (user) {
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_SECRET);
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            msg: "Wrong credentials"
+        })
+    }
+    console.log(user);
 })
 app.post('/todo', (req, res) => {
+
 
 })
 app.get('/todos', (req, res) => {
 
 })
-
-
-
 
 
 app.listen(port, () => {
