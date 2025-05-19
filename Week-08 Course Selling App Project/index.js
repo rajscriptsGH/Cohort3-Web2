2a71a38683748d8de80c9687679e8fdb05f3ce2a
rajyadav63 <vector36razz@gmail.com>
2025-05-19 17:46:03 +0530

    course and user routes updated

--- Week-08 Course Selling App Project/index.js
+++ Week-08 Course Selling App Project/index.js
@@ -1,41 +1,16 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import userRouter from './routes/user'
+import courseRouter from './routes/courses'
 const app = express()
 const port = 3000;
 
 app.use(express.json())
 
-app.post('/user/signup', (req, res) => {
-    res.json({
-        msg: "signup endpoint"
-    })
-})
-app.post('/user/signin', (req, res) => {
-    res.json({
-        msg: "signin endpoint"
-    })
-})
-app.post('/course', (req, res) => {
-    res.json({
-        msg: "course endpoint"
-    })
-})
-app.post('/course/purchases', (req, res) => {
-    res.json({
-        msg: "course purchases endpoint"
-    })
-})
-app.post('/user/purchases', (req, res) => {
-    res.json({
-        msg: "user purchases endpoint"
-    })
-})
-app.post('/delete/course', (req, res) => {
-    res.json({
-        msg: "delete course' endpoint"
-    })
-})
+
+app.post('/user', userRouter)
+app.post('/course', courseRouter)
 
 
 
