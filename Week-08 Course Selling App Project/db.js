b00bac60f476d7867bcaf0c6476e4dfbf8cb39ef
rajyadav63 <vector36razz@gmail.com>
2025-05-19 23:39:57 +0530

    schema added to db

--- Week-08 Course Selling App Project/db.js
+++ Week-08 Course Selling App Project/db.js
@@ -1,6 +1,5 @@
-import express from 'express'
+
 import mongoose from 'mongoose'
-import jwt from 'jsonwebtoken'
 
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId
@@ -19,18 +18,23 @@ const adminSchema = new Schema({
     firstName: String,
     lastName: String
 })
+
 const courseSchema = new Schema({
     _id: ObjectId,
-    email: String,
-    password: String,
-    firstName: String,
-    lastName: String
+    title: String,
+    desc: String,
+    price: Number,
+    imgUrl: String,
+    adminId: ObjectId
 })
 
 const purchaseSchema = new Schema({
     _id: ObjectId,
-    email: String,
-    password: String,
-    firstName: String,
-    lastName: String
+    courseId: ObjectId,
+    userId: ObjectId
 })
+
+const userModel = mongoose.Model('user', userSchema)
+const adminModel = mongoose.Model('user', adminModel)
+const courseModel = mongoose.Model('user', courseModel)
+const purchaseModel = mongoose.Model('user', purchaseModel)
