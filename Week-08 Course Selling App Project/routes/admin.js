c819371b8ab99f3db90504956a1c7f613aa1749d
rajyadav63 <vector36razz@gmail.com>
2025-05-24 00:17:37 +0530

    admin signin updated

--- Week-08 Course Selling App Project/routes/admin.js
+++ Week-08 Course Selling App Project/routes/admin.js
@@ -1,21 +1,80 @@
 import { Router } from "express";
 const adminRouter = Router()
-
-
+import { adminModel } from "../db.js";
+import bcrypt from 'bcrypt'
+import z from 'zod'
+import jwt from 'jsonwebtoken'
+const JWT_ADMIN_PASSWORD = 'ldkfldffhid'
 
 adminRouter.post('/signup', async (req, res) => {
+    const { email, password, firstName, lastName } = req.body;
+
+    //1. Use zod for validation
+    const reqBody = z.object({
+        email: z.string().email().includes('@'),
+        password: z.string().min(6).max(20),
+        firstName: z.string().min(3).max(50),
+        lastName: z.string().min(3).max(50)
+    })
+
+    const parseBodyWithSucess = reqBody.safeParse(req.body)
+    if (!parseBodyWithSucess.success) {
+        res.status(403).json({
+            error: parseBodyWithSucess.error
+        })
+        return
+    }
+
+    //2. Hash the password
+    const hashPassword = await bcrypt.hash(password, 5)
+    try {
+        await adminModel.create({
+            email: email,
+            password: hashPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (e) {
+        console.log("Error: ", e);
+    }
+
     res.json({
         msg: "Admin signed up"
     })
 
 })
-adminRouter.post('/signin', (req, res) => {
+adminRouter.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    //1. Find admin by email only
+    const admin = await adminModel.findOne({
+        email: email
+    })
+    if (!admin) {
+        return res.status(403).json({
+            msg: "Invalid admin"
+        })
+    }
+
+    //2. Check passward using hash
+    const isPasswordValid = await bcrypt.compare(password, admin.password)
+    if (!isPasswordValid) {
+        return res.status(403).json({
+            msg: "Invalid password"
+        })
+    }
+
+    //3. sign jwt token
+    const token = jwt.sign({
+        id: admin._id
+    }, JWT_ADMIN_PASSWORD)
+
     res.json({
+        token: token,
         msg: "Admin signed in"
     })
-})
 
-// adminRouter.use(checkAdminMiddleware)
+})
 
 adminRouter.post('/courses', (req, res) => {
     res.json({
