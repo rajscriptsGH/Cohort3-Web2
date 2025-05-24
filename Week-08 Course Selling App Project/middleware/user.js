1c1352ae0562288930e2f049e5a22a975086ba81
rajyadav63 <vector36razz@gmail.com>
2025-05-24 23:17:54 +0530

    middleware for user and admin added

--- Week-08 Course Selling App Project/middleware/user.js
+++ Week-08 Course Selling App Project/middleware/user.js
@@ -7,9 +7,9 @@ export function userMiddleware(req, res, next) {
     const decoded = jwt.verify(token, JWT_USER_PASSWORD)
 
     if (decoded) {
-        req.userId = decoded.id;
+        req.userId = decoded.id;   // this is came from signin decoded function
         next()
-    }else{
+    } else {
         res.status(403).json({
             msg: "u are not signed up"
         })
