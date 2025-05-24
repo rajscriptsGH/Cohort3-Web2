914e9d72bd6ebbcf0f27066d7a4336b979efaf8e
rajyadav63 <vector36razz@gmail.com>
2025-05-24 23:16:57 +0530

    middleware for user and admin added

--- Week-08 Course Selling App Project/middleware/admin.js
+++ Week-08 Course Selling App Project/middleware/admin.js
@@ -1,5 +1,17 @@
 import jwt from 'jsonwebtoken'
+import { JWT_ADMIN_PASSWORD } from '../config.js'
 
- function adminMiddleware(req, res, next) {
+export function adminMiddleware(req, res, next) {
+    const token = req.headers.token
 
+    const decoded = jwt.verify(token, JWT_ADMIN_PASSWORD)
+
+    if (decoded) {
+        req.adminId = decoded.id;
+        next()
+    } else {
+        res.status(403).json({
+            msg: "admin are not signed up"
+        })
+    }
 }
--- Week-08 Course Selling App Project/middleware/user.js
+++ Week-08 Course Selling App Project/middleware/user.js
@@ -1,5 +1,17 @@
 import jwt from 'jsonwebtoken'
+import { JWT_USER_PASSWORD } from '../config.js'
 
 export function userMiddleware(req, res, next) {
+    const token = req.headers.token
 
+    const decoded = jwt.verify(token, JWT_USER_PASSWORD)
+
+    if (decoded) {
+        req.userId = decoded.id;
+        next()
+    }else{
+        res.status(403).json({
+            msg: "u are not signed up"
+        })
+    }
 }
