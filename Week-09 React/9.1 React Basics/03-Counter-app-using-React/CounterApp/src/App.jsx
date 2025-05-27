a9747c3b918f684cd9b3b02f5f865a9d737f95bc
rajyadav63 <vector36razz@gmail.com>
2025-05-27 17:09:29 +0530

    counter app

--- Week-09 React/9.1 React Basics/03-Counter-app-using-React/CounterApp/src/App.jsx
+++ Week-09 React/9.1 React Basics/03-Counter-app-using-React/CounterApp/src/App.jsx
@@ -1,11 +1,17 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function onClickPress(){
+    setCount(count + 1)
+  }
 
   return (
     <div>
-      <h1>hello React</h1>
+      <button id='btn' onClick={onClickPress}>Counter {count}</button>
     </div>
 
   )
