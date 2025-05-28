b1dd7d1e9f5d3502b87fd51f4ff4cdcbe2f2d36b
rajyadav63 <vector36razz@gmail.com>
2025-05-28 23:05:40 +0530

    useSate

--- Week-09 React/9.2 React Hooks/01-useState/src/App.jsx
+++ Week-09 React/9.2 React Hooks/01-useState/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
@@ -5,8 +6,30 @@ function App() {
   return (
     <div>
     <h1>useState</h1>
+    <Counter />
    </div>
   )
+}
+
+function Counter(){
+  const [count, setCount] =  useState(0)
+
+  function increasCount(){
+    setCount(count + 1)
+  }
+  function decreaseCount(){
+    setCount(count - 1)
+  }
+  function resetCount(){
+    setCount(0)
+  }
+
+  return <div>
+    <h1>{count}</h1>
+    <button onClick={increasCount}>IncreaseCount</button>
+    <button onClick={decreaseCount}>DecreaseCount</button>
+    <button onClick={resetCount}>ResetCount</button>
+  </div>
 }
 
 export default App
