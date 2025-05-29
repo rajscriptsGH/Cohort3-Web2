365ef4845750feb853a00aa33fb8dce70021c829
rajyadav63 <vector36razz@gmail.com>
2025-05-29 08:17:40 +0530

    useEffect done

--- Week-09 React/9.2 React Hooks/02-useEffect/src/App.jsx
+++ Week-09 React/9.2 React Hooks/02-useEffect/src/App.jsx
@@ -1,9 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
+  return <div>
+
    <h1>useEffect</h1>
-  )
+   <Counter />
+  </div>
+  
 }
 
+function Counter(){
+const [count, setCount] = useState(0)
+
+console.log("counter");
+
+
+//gaurd interval from re-rendering
+useEffect(()=>{
+  setInterval(()=>{
+    setCount(function(count){
+      return count+1;
+    });
+}, 1000);
+console.log("Mounted");
+
+},[])
+
+  // function increaseCount(){
+  //   setCount(count + 1)
+  // }
+
+  return <div>
+    <h1>{count}</h1>
+    {/* <button onClick={increaseCount}>IncreaseCount</button> */}
+  </div>
+}
 export default App
