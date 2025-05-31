dea8c53fe40aa4177b5ec1323d32aff44b2c3bbc
rajyadav63 <vector36razz@gmail.com>
2025-06-01 02:10:22 +0530

    conditional rendering updated

--- Week-09 React/Offline/9.4  React Basic (Recorded-1)/03-Conditional Rendering/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-1)/03-Conditional Rendering/src/App.jsx
@@ -1,12 +1,79 @@
 import './App.css'
 
 function App() {
-
   return (
-    <div>
-      Conditional rendering
+    <div style={{
+      backgroundColor: 'grey',
+      height: '100vh',
+      width: '100%',
+      display: 'flex',
+      // flexDirection: 'column',
+      // alignItems: 'center',
+    }}>
+      <div style={{ margin: 6, marginRight: 20 }}>
+        <PostComponent
+          title="10xDevs"
+          followers='23,333 followers'
+          minute="12m"
+          content="orem ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur adipis"
+
+        />
+        <br />
+        <PostComponent
+          title="Harkirat"
+          followers='600k followers'
+          minute="12m"
+          content="ipsum dolor sit amet consecteturorem ipsum dolor sit amet consectetur adipis"
+        />
+        <br />
+      </div>
     </div>
   )
 }
 
+
+
+function PostComponent({ title, followers, minute, content }) {
+  return <div style={{
+    width: 250,
+    backgroundColor: "black",
+    color: 'white',
+    borderRadius: 5,
+    borderColor: "grey",
+    borderWidth: 1,
+    display: 'flex',
+    justifyContent: 'center',
+
+  }}>
+    <div>
+      <div style={{ display: 'flex', marginBottom: 10 }}>
+        <div>
+          <img src="https://t3.ftcdn.net/jpg/14/60/58/40/240_F_1460584064_SZ7FhrwKuvrOIomUWrMedQkAsYb4nrT9.jpg" style={{
+            height: 50,
+            width: 50,
+            borderRadius: 20
+          }} />
+        </div>
+        <div style={{ marginLeft: 12, textAlign: 'left' }}>
+          <div style={{ fontSize: 30 }}>
+            {title}
+          </div>
+          <div style={{ fontSize: 15 }}>
+            {followers}
+          </div>
+          <div style={{ fontSize: 15 }}>
+            {minute}
+          </div>
+
+        </div>
+      </div>
+      <div style={{ fontSize: 15, padding: 5 }}>
+        {content}
+      </div>
+    </div>
+  </div>
+}
+
 export default App
+
+
