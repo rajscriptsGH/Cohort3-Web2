85902212d5dc26485f11d9933ad35499d709aaa7
rajyadav63 <vector36razz@gmail.com>
2025-06-01 18:01:03 +0530

    Notification post

--- Week-09 React/Offline/9.4  React Basic (Recorded-1)/04-useState/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-1)/04-useState/src/App.jsx
@@ -1,10 +1,50 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
+    <div style={{ backgroundColor: "black", color: '#fff', margin: 40 }}>
+      <ToggleMessage />
+      <ToggleMessage />
+      <Notification
+        title='request'
+      />
+      <Notification
+        title='notification'
+      />
+      <Notification
+        title='msg'
+      />
+    </div>
+  )
+}
+
+function Notification({ title }) {
+  const [notification, setNotification] = useState(0)
+
+  function increaseNotis() {
+    setNotification(notification + 1)
+  }
+
+  return (
     <div>
-      <h1>useState</h1>
+      <p>{notification}</p>
+      <button onClick={increaseNotis}>{title}</button>
+    </div>
+  )
+}
+const ToggleMessage = () => {
+  const [isVisible, setIsVisibel] = useState(true)
+
+  function toggle() {
+    setIsVisibel(!isVisible)
+  }
+
+  return (
+    <div>
+      <button onClick={toggle}>Toggle Message</button>
+      {isVisible && <p>This is conditionallly rendered</p>}
     </div>
   )
 }
