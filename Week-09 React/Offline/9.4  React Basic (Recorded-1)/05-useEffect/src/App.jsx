c0bc143ddd77de0aa2dd0b35dee78d7ce2c2f8d3
rajyadav63 <vector36razz@gmail.com>
2025-06-02 23:16:03 +0530

    useEffect for linkedin bar

--- Week-09 React/Offline/9.4  React Basic (Recorded-1)/05-useEffect/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-1)/05-useEffect/src/App.jsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import './App.css'
-import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTab, setCurrentTab] = useState("feed")
 
-  function increaseCount() {
-    setCount(count => count + 1)
-  }
 
-  useEffect(() => {
-    setInterval(increaseCount, 1000)
-  }, [])
 
   return (
     <div>
-      {count}
-    </div>
+      <button onClick={() => {
+        setCurrentTab('feed')
+      }} style={{ color: currentTab == "feed" ? "red" : "white", marginLeft: 5 }}>Feed</button>
+      <button onClick={() => {
+        setCurrentTab('notification')
+      }} style={{ color: currentTab == "notification" ? "red" : "white", marginLeft: 5 }}>Notifications</button>
+      <button onClick={() => {
+        setCurrentTab('messages')
+      }} style={{ color: currentTab == "messages" ? "red" : "white", marginLeft: 5 }}>Messages</button>
+      <button onClick={() => {
+        setCurrentTab('jobs')
+      }} style={{ color: currentTab == "jobs" ? "red" : "white", marginLeft: 5 }}>Jobs</button>
+      <button onClick={() => {
+        setCurrentTab('request')
+      }} style={{ color: currentTab == "request" ? "red" : "white", marginLeft: 5 }}>Request</button>
+    </div >
   )
 }
 
