bd2d18bfd7705769fd3dc2411568a95dd8bd6728
rajyadav63 <vector36razz@gmail.com>
2025-06-02 18:10:46 +0530

    useEffect

--- Week-09 React/Offline/9.4  React Basic (Recorded-1)/05-useEffect/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-1)/05-useEffect/src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function increaseCount() {
+    setCount(count => count + 1)
+  }
+
+  useEffect(() => {
+    setInterval(increaseCount, 1000)
+  }, [])
 
   return (
     <div>
-
+      {count}
     </div>
   )
 }
