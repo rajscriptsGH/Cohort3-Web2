910ae6559ceefd298699d94ea0430d9888b5e06a
rajyadav63 <vector36razz@gmail.com>
2025-06-04 23:47:19 +0530

    Function and Class based

--- Week-09 React/Offline/9.4  React Basic (Recorded-2)/04-Class-based-Function-based/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-2)/04-Class-based-Function-based/src/App.jsx
@@ -1,4 +1,5 @@
 
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
@@ -6,8 +7,45 @@ function App() {
   return (
     <div>
       <h1>Class based vs Function based</h1>
+      <FunctionalCounter />
+      <ClassCounter />
     </div>
   )
 }
+
+//Class based component
+
+// class ClassCounter extends Component {
+//   state = { count: 0 };
+
+//   increment = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <p>Count: {this.state.count}</p>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
+
+
+const FunctionalCounter = () => {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count => count + 1)
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+    </div>
+  );
+};
 
 export default App
