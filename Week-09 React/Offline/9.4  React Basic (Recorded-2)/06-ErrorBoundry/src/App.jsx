471da865f134f4538631753417c103374a150135
rajyadav63 <vector36razz@gmail.com>
2025-06-05 00:56:17 +0530

    Error Boundry

--- Week-09 React/Offline/9.4  React Basic (Recorded-2)/06-ErrorBoundry/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-2)/06-ErrorBoundry/src/App.jsx
@@ -1,11 +1,58 @@
-
+// import { ErrorBoundry } from './ErrorBoundry.jsx'
+import React from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <div><h1>Error Boundry</h1></div>
+    <div>
+      <h1>Error Boundry</h1>
+      <ErrorBoundary>
+        <Card1 />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Card2 />
+      </ErrorBoundary>
+    </div>
+
   )
 }
 
+function Card1() {
+  // throw new Error('Error while rendering');
+
+  return <div style={{ backgroundColor: 'blue', padding: 14, borderRadius: 10, margin: 8 }}>
+    hello
+  </div>
+}
+function Card2() {
+  // throw new Error('Error while rendering');
+  return <div style={{ backgroundColor: 'blue', padding: 14, borderRadius: 10 }}>
+    hello dear
+  </div>
+}
+
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    console.error("Error caught:", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 export default App
