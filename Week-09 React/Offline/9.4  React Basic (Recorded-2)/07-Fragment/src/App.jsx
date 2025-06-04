f2ee03183ef3c781efa145c39878652a662ece1d
rajyadav63 <vector36razz@gmail.com>
2025-06-05 01:04:38 +0530

    Fragment in React

--- Week-09 React/Offline/9.4  React Basic (Recorded-2)/07-Fragment/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-2)/07-Fragment/src/App.jsx
@@ -6,7 +6,19 @@ function App() {
   return (
     <div>
       <h1>Fragment in React</h1>
+      <p>Fragment in React</p>
+      <MyComponent />
     </div>
+  )
+}
+
+function MyComponent() {
+
+  return (
+    <>
+      <div>Hii</div>
+      <div>Hii there</div>
+    </>
   )
 }
 
