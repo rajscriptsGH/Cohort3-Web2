452ef80f55b71eac4714c0e9cb88a21610962960
rajyadav63 <vector36razz@gmail.com>
2025-06-05 00:05:19 +0530

    Life cycle events

--- Week-09 React/Offline/9.4  React Basic (Recorded-2)/05-LifeCycle-events/src/App.jsx
+++ Week-09 React/Offline/9.4  React Basic (Recorded-2)/05-LifeCycle-events/src/App.jsx
@@ -4,8 +4,36 @@ import './App.css'
 function App() {
 
   return (
-    <div>Life cycle events</div>
+    <div>
+      <h4>Life cycle events</h4>
+      <MyComponent />
+    </div>
   )
+}
+
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Component mounted or count updated');
+
+  }, [count]); // Runs on mount and when count changes
+
+  useEffect(() => {
+    console.log('Component mounted');
+    return () => {
+      console.log('Component will unmount');
+    };
+  }, [])
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
 }
 
 export default App
