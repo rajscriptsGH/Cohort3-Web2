264f6ebf11059b533de17cf75e8d985721320767
rajyadav63 <vector36razz@gmail.com>
2025-06-07 04:17:41 +0530

    Routes added

--- Week-10 React(2)/10.1 SPAs & Routing/01-Routing/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/01-Routing/src/App.jsx
@@ -1,11 +1,39 @@
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
-    <div>Hii there</div>
+    <BrowserRouter>
+      <Link to={'/'}>Home</Link >
+      |
+      <Link to={"/project"}>Project </Link >
+      |
+      <Link to={"/book"}>Call</Link >
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/book" element={<Call />} />
+      </Routes>
+    </BrowserRouter>
   )
+}
+
+function Home() {
+  return <div>
+    Home section
+  </div>
+}
+function Project() {
+  return <div>
+    Project section
+  </div>
+}
+function Call() {
+  return <div>
+    Book a Call section
+  </div>
 }
 
 export default App
