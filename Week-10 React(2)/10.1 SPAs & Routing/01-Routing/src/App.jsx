7bd471c87704e40ec410f762648e64b925206f3f
rajyadav63 <vector36razz@gmail.com>
2025-06-07 04:33:38 +0530

    useNavigate hook added

--- Week-10 React(2)/10.1 SPAs & Routing/01-Routing/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/01-Routing/src/App.jsx
@@ -1,17 +1,19 @@
 
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
+    //act as a parent of Routes
     <BrowserRouter>
+      {/* //Link is a alternative of a tag to prevent from loading pages in react */}
       <Link to={'/'}>Home</Link >
       ||
       <Link to={"/project"}>Project </Link >
       ||
       <Link to={"/book"}>Call</Link >
-      <Routes>
+      <Routes>     //act as a parent of Route
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
         <Route path="/book" element={<Call />} />
@@ -31,8 +33,15 @@ function Project() {
   </div>
 }
 function Call() {
+  //usenavigate is used to navigate b/n pages on website
+  const navigate = useNavigate()
+
+  function backHome() {
+    navigate('/')
+  }
   return <div>
     Book a Call section
+    <button onClick={backHome}>Back to Home</button>
   </div>
 }
 
