3074eac29953de95852a129fa2bd666ff7429591
rajyadav63 <vector36razz@gmail.com>
2025-06-07 20:06:00 +0530

    Notes added

--- Week-10 React(2)/10.1 SPAs & Routing/04-Stopwatch-using-useRef/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/04-Stopwatch-using-useRef/src/App.jsx
@@ -1,10 +1,18 @@
 
 import { useState, useRef } from 'react'
+// useState: Manages state (here, the current count).
+
+// useRef: Stores a mutable reference that doesn’t trigger a re-render when updated. You’ll use it to store the timer ID.
 import './App.css'
 
 function App() {
   const [currentValue, setCurrentvalue] = useState(1)
   let timer = useRef();
+  //  currentValue: A state variable holding the current number on the stopwatch.
+
+  // setCurrentvalue: A function to update currentValue.
+
+  // useRef(): Creates a reference object (initially undefined). You’ll store the timer ID in timer.current.
 
 
   function startCount() {
@@ -12,12 +20,17 @@ function App() {
       setCurrentvalue(count => count + 1)
     }, 1000)
     timer.current = value
-
   }
+  // setInterval: Runs a function every 1 second (1000 ms).
+
+  // setCurrentvalue(count => count + 1): Updates the state by incrementing the previous value.
+
+  // timer.current = value: Stores the interval ID in the ref so we can stop it later.
+
   function stopCount() {
     clearInterval(timer.current)
-
   }
+  // clearInterval(timer.current): Stops the timer using the ID stored in timer.current.
 
   return (
     <div>
