879c0b67dabdfe382f3df86447b721ff52a5c4d9
rajyadav63 <vector36razz@gmail.com>
2025-06-07 19:57:44 +0530

    stopWatch using useRef

--- Week-10 React(2)/10.1 SPAs & Routing/04-Stopwatch-using-useRef/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/04-Stopwatch-using-useRef/src/App.jsx
@@ -1,13 +1,31 @@
 
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
+  const [currentValue, setCurrentvalue] = useState(1)
+  let timer = useRef();
+
+
+  function startCount() {
+    let value = setInterval(() => {
+      setCurrentvalue(count => count + 1)
+    }, 1000)
+    timer.current = value
+
+  }
+  function stopCount() {
+    clearInterval(timer.current)
+
+  }
 
   return (
     <div>
       <h1>StopWatch using useRef</h1>
-
-
+      {currentValue}
+      <br />
+      <button onClick={startCount}>Start</button>
+      <button onClick={stopCount}>Stop</button>
     </div>
   )
 }
