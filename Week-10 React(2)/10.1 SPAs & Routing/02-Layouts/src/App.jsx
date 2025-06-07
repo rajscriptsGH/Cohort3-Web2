31879dfae10c1e555c64bb2ef2395f261b28171e
rajyadav63 <vector36razz@gmail.com>
2025-06-07 17:13:22 +0530

    layouts in React

--- Week-10 React(2)/10.1 SPAs & Routing/02-Layouts/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/02-Layouts/src/App.jsx
@@ -1,11 +1,90 @@
 
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import './App.css'
 
 function App() {
 
   return (
-    <div>Layouts</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/book" element={<Call />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
+}
+
+function Layout() {
+  return <div style={{ height: '100vh' }}>
+    <Header />
+
+    <div style={{ height: '90vh', backgroundColor: "purple" }}>
+      <Outlet />
+    </div>
+
+    <div style={{ backgroundColor: "green" }}>
+      footer
+    </div>
+  </div>
+}
+
+function Header() {
+  return <div>
+    <Link to={"/"}>LandingPage</Link >
+    ||
+    <Link to={"/home"}>Home</Link >
+    ||
+    <Link to={"/project"}>Project </Link >
+    ||
+    <Link to={"/book"}>Call</Link >
+  </div>
+}
+
+
+function LandingPage() {
+  return <div>
+    landing page
+  </div>
+}
+function Home() {
+  return <div>
+    Home section
+  </div>
+}
+function Project() {
+  return <div>
+    Project section
+  </div>
+}
+function Call() {
+  //usenavigate is used to navigate b/n pages on website
+  const navigate = useNavigate()
+
+  function backHome() {
+    navigate('/')
+  }
+  return <div>
+    Book a Call section
+    <button onClick={backHome}>Back to Home</button>
+  </div>
+}
+
+function ErrorPage() {
+  const navigate = useNavigate()
+
+  function backHome() {
+    navigate('/')
+  }
+  return <div>
+
+    <p> Sorry, page not found</p>
+    <button onClick={backHome}>Back to Home</button>
+  </div>
 }
 
 export default App
