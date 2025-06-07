1d87829f88d2c8cd0de4c3b46fe1b4ed1dba332b
rajyadav63 <vector36razz@gmail.com>
2025-06-07 18:16:31 +0530

    useRef updated

--- Week-10 React(2)/10.1 SPAs & Routing/03-useRef-Hook/src/App.jsx
+++ Week-10 React(2)/10.1 SPAs & Routing/03-useRef-Hook/src/App.jsx
@@ -1,9 +1,19 @@
+import { useRef } from 'react'
 import './App.css'
 
 function App() {
+  const inputRef = useRef()
 
+  function focusOnInput() {
+    inputRef.current.focus()
+  }
   return (
-   <div>useRef Hook</div>
+    < div >
+      Signup < br />
+      <input ref={inputRef} type="text" /> <br /> <br />
+      <input type="text" /> <br />
+      <button onClick={focusOnInput}>Submit</button>
+    </div>
   )
 }
 
