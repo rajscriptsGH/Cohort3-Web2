2cc70ac0313ef4cd64c8f16777e91409f07d3304
rajyadav63 <vector36razz@gmail.com>
2025-06-09 04:54:14 +0530

    Testing Context API

---
+++ Week-10 React(2)/10.2 Context API/04-Testing-Context-API/src/App.jsx
@@ -0,0 +1,49 @@
+
+import { useState, useContext, createContext } from "react"
+
+const CountContext = createContext()
+
+function CountContextProvider({ children }) {
+  const [count, setCount] = useState(0)
+
+  return <CountContext.Provider value={{ count, setCount }}>
+    {children}
+  </CountContext.Provider>
+}
+
+function App() {
+
+  return (
+    <div>
+      <Parent />
+    </div>
+  )
+}
+function Parent() {
+  return <div>
+    <CountContextProvider>
+      <Value />
+      <Increase />
+      <Decrease />
+    </CountContextProvider>
+  </div>
+}
+
+function Value() {
+  const { count } = useContext(CountContext)
+  return <div>
+    <p>Count: {count}</p>;
+  </div>
+}
+function Increase() {
+  const { count, setCount } = useContext(CountContext)
+
+  return <button onClick={() => setCount(count + 1)}>Increase</button>
+}
+
+function Decrease() {
+  const { count, setCount } = useContext(CountContext)
+  return <button onClick={() => setCount(count - 1)}>Decrease</button>
+}
+
+export default App
