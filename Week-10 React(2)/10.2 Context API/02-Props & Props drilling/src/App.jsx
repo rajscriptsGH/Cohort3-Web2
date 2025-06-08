f587305e845e0417579f5b335322fad7ed48ebf8
rajyadav63 <vector36razz@gmail.com>
2025-06-09 00:21:52 +0530

    Notes added to props drilling

--- Week-10 React(2)/10.2 Context API/02-Props & Props drilling/src/App.jsx
+++ Week-10 React(2)/10.2 Context API/02-Props & Props drilling/src/App.jsx
@@ -1,12 +1,13 @@
 
 import { useState } from 'react'
-import './App.css'
 
 function App() {
+  // State bulbOn: Initialized to true, meaning the bulb is initially ON.
+  // setBulbOn: Function used to change the state.
   const [bulbOn, setBulbOn] = useState(true)
-
   return (
     <div>
+      {/* // The Light component is rendered and given the bulbOn state and setBulbOn updater as props. */}
       <Light bulbOn={bulbOn} setBulbOn={setBulbOn} />
     </div>
   )
@@ -18,15 +19,28 @@ function Light({ bulbOn, setBulbOn }) {
     <LightSwitch bulbOn={bulbOn} setBulbOn={setBulbOn} />
   </div>
 }
+// Light receives the state and its updater from App.
 
+// It renders two child components:
+
+{/* <LightBulb />: to display whether the bulb is ON or OFF. */ }
+
+{/* <LightSwitch />: to toggle the bulb’s state. */ }
+
+// This is a pure display component.
 function LightBulb({ bulbOn }) {
   return <div>
+    {/* // "Bulb is on" if bulbOn is true. */}
+    {/* // "Bulb is off" if bulbOn is false. */}
     {bulbOn ? "Bulb is on" : "Bulb is off"}
   </div>
-
 }
+// It receives the bulbOn prop and displays a message:
+
+
 
 function LightSwitch({ bulbOn, setBulbOn }) {
+  // toggle() function flips the state using !bulbOn.
   function toggle() {
     setBulbOn(!bulbOn)
   }
