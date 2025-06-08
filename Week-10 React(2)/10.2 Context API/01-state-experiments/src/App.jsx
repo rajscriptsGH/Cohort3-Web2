cf374afd8d9ae1e5ed012e9ddc01a2f881b771df
rajyadav63 <vector36razz@gmail.com>
2025-06-08 19:39:15 +0530

    State experiment

--- Week-10 React(2)/10.2 Context API/01-state-experiments/src/App.jsx
+++ Week-10 React(2)/10.2 Context API/01-state-experiments/src/App.jsx
@@ -1,11 +1,37 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <div>State Experiments</div>
+    <div>
+      <LightBulb />
+    </div>
   )
 }
 
+function LightBulb() {
+  return <div>
+    <BulbState />
+    <ToggleBulbState />
+  </div>
+}
+
+function BulbState() {
+  const [bulbOn, setBulbOn] = useState(true)
+
+  return <div>
+    {bulbOn ? "Bulb is on" : "Bulb is off"}
+  </div>
+
+}
+
+function ToggleBulbState() {
+
+  return <div>
+    <button>Toggle the bulb</button>
+  </div>
+
+}
 export default App
