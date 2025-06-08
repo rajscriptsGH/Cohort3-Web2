2a1ac427804c3de1d6fbc8753b686150050c399b
rajyadav63 <vector36razz@gmail.com>
2025-06-08 23:55:39 +0530

    Props drilling

--- Week-10 React(2)/10.2 Context API/02-Props & Props drilling/src/App.jsx
+++ Week-10 React(2)/10.2 Context API/02-Props & Props drilling/src/App.jsx
@@ -1,11 +1,40 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
-    <div>Props & Prop drilling</div>
+    <div>
+      <LightBulb />
+    </div>
   )
 }
 
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true)
+  return <div>
+    <BulbState bulbOn={bulbOn} />
+    <ToggleBulbState bulbOn={bulbOn} setBulbOn={setBulbOn} />
+  </div>
+}
+
+function BulbState({ bulbOn }) {
+
+  return <div>
+    {bulbOn ? "Bulb is on" : "Bulb is off"}
+  </div>
+
+}
+
+function ToggleBulbState({ bulbOn, setBulbOn }) {
+  function toggle() {
+    setBulbOn(!bulbOn)
+  }
+
+  return <div>
+    <button onClick={toggle}>Toggle the bulb</button>
+  </div>
+
+}
 export default App
