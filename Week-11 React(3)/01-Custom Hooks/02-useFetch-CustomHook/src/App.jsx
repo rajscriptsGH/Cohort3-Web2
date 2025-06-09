f2207ee1bb7fb6a92e9946cd1127ceeea0bce35f
rajyadav63 <vector36razz@gmail.com>
2025-06-10 01:00:46 +0530

    useFetch Custom Hook

---
+++ Week-11 React(3)/01-Custom Hooks/02-useFetch-CustomHook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+
+
+//Custom Hook
+function useCounter() {
+  const [count, setCount] = useState(0)
+
+  function increase() {
+    setCount(count + 1)
+  }
+  return {
+    count: count,
+    increase: increase
+  }
+}
+
+function App() {
+
+  return (
+    <div>
+      useFetch
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const { count, increase } = useCounter()
+
+  return <div>
+    <button onClick={increase}>Increase: {count}</button>
+  </div>
+
+}
+
+export default App
