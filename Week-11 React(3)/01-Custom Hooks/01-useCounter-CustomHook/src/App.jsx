cd7c236b3b0b4fb7bc4a84a3c8b090982a72b5c7
rajyadav63 <vector36razz@gmail.com>
2025-06-10 00:48:47 +0530

    Custome Hook in Component

--- Week-11 React(3)/01-Custom Hooks/01-useCounter-CustomHook/src/App.jsx
+++ Week-11 React(3)/01-Custom Hooks/01-useCounter-CustomHook/src/App.jsx
@@ -1,15 +1,7 @@
 import { useState } from "react"
 
-function App() {
-  const { count, increase } = useCounter()
 
-  return (
-    <div>
-      <button onClick={increase}>Increase: {count}</button>
-    </div>
-  )
-}
-
+//Custom Hook
 function useCounter() {
   const [count, setCount] = useState(0)
 
@@ -20,6 +12,26 @@ function useCounter() {
     count: count,
     increase: increase
   }
+}
+
+function App() {
+
+  return (
+    <div>
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  )
+}
+
+function Counter() {
+  const { count, increase } = useCounter()
+
+  return <div>
+    <button onClick={increase}>Increase: {count}</button>
+  </div>
+
 }
 
 export default App
