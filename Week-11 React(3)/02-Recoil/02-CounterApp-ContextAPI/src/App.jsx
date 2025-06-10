ba0f23dd72f12d919134214ab83516fcd39c3b8e
rajyadav63 <vector36razz@gmail.com>
2025-06-11 02:59:40 +0530

    Counter app using context api

---
+++ Week-11 React(3)/02-Recoil/02-CounterApp-ContextAPI/src/App.jsx
@@ -0,0 +1,35 @@
+
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <Counter />
+    </div>
+  )
+}
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  return <div>
+    {count}
+    <IncreaseCounter setCount={setCount} />
+    <DcreaseCounter setCount={setCount} />
+  </div>
+}
+
+function IncreaseCounter({ setCount }) {
+  return <div>
+    <button onClick={() => { setCount(count => count + 1) }}>Increase</button>
+  </div>
+}
+function DcreaseCounter({ setCount }) {
+  return <div>
+    <button onClick={() => { setCount(count => count - 1) }}>Decrease</button>
+  </div>
+}
+
+export default App
