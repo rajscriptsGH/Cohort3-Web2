c94c6dc1df00c57b5f121bcdc99f9806a1578e91
rajyadav63 <vector36razz@gmail.com>
2025-06-13 11:17:12 +0530

    Tailwind Basics Done

--- Week-13 Tailwind CSS/01-Tailwind (Part1)/01-Tailwind-Basics/src/components/FlexTest.jsx
+++ Week-13 Tailwind CSS/01-Tailwind (Part1)/01-Tailwind-Basics/src/components/FlexTest.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FlexTest() {
     return (
         <div className='sm:flex justify-between'>
-            <div className='bg-green-400'>
+            <div className='bg-green-400 text-2xl rounded'>
                 This is the first children
             </div>
             <div className='bg-red-400'>
