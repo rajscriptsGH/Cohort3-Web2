2a68dc6afd23c300651a2bfad8f60e18109bd78d
rajyadav63 <vector36razz@gmail.com>
2025-06-17 08:34:22 +0530

    Tailwind added

--- Week-13 Tailwind CSS/02-Tailwind (Part2)/src/App.jsx
+++ Week-13 Tailwind CSS/02-Tailwind (Part2)/src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Sidebar from './components/Sidebar'
 
 function App() {
 
   return (
     <div>
       Tailwind Part2
+      <Sidebar />
     </div>
   )
 }
---
+++ Week-13 Tailwind CSS/02-Tailwind (Part2)/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className=''>
+            <div>
+                Sidebar
+            </div>
+            <div>
+                Content
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
