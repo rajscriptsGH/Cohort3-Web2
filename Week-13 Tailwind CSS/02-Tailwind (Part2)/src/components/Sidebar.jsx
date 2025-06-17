897cd60bceb72bcb9d4c005559801c33c4dd3c9c
rajyadav63 <vector36razz@gmail.com>
2025-06-17 22:47:10 +0530

    sidebar

--- Week-13 Tailwind CSS/02-Tailwind (Part2)/src/components/Sidebar.jsx
+++ Week-13 Tailwind CSS/02-Tailwind (Part2)/src/components/Sidebar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div className=''>
-            <div>
+        <div className='flex justify-center items-center gap-2'>
+            <div className='bg-blue-600 h-screen sm:w-80 w-4 transition-all duration-2000'>
                 Sidebar
             </div>
-            <div>
+            <div className='bg-red-500 h-screen flex-1'>
                 Content
             </div>
         </div>
