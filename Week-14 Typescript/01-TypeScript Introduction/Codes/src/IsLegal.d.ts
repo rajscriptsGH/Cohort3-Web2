241bf2258c42cd9373656016172245cbe42374bb
rajyadav63 <vector36razz@gmail.com>
2025-06-25 00:08:30 +0530

    3rd islegal

---
+++ Week-14 Typescript/01-TypeScript Introduction/Codes/src/IsLegal.d.ts
@@ -0,0 +1,1 @@
+declare function islegal(age: number): boolean;
---
+++ Week-14 Typescript/01-TypeScript Introduction/Codes/src/IsLegal.ts
@@ -0,0 +1,10 @@
+
+function islegal(age:number){
+    if(age>=18){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(islegal(2));
