f2030f8a4dc82625206d15cccabf31c96ded6bef
rajyadav63 <vector36razz@gmail.com>
2025-06-27 08:25:48 +0530

    function is ts added

---
+++ Week-14 Typescript/02-TypeScript Types & Interface /Codes/src/01-function.ts
@@ -0,0 +1,20 @@
+function sum(a:number, b:number): number{
+    return a+b;
+}
+
+const ans = sum(2,3)
+console.log(ans);
+
+// a,b: number → parameter with a type
+
+// : number after the parenthesis → return type of the function
+
+function greet(name: string): string {
+  return "Hello, " + name;
+}
+
+console.log(greet("Razz")); // Output: Hello, Razz
+
+// name: string → parameter with a type
+
+// : string after the parenthesis → return type of the function
