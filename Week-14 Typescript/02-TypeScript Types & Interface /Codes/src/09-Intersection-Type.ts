5023c5525e3e10ef6e9524529dc9b0def1bfcf27
rajyadav63 <vector36razz@gmail.com>
2025-06-28 13:24:02 +0530

    intersection type

---
+++ Week-14 Typescript/02-TypeScript Types & Interface /Codes/src/09-Intersection-Type.ts
@@ -0,0 +1,25 @@
+type Employee = {
+    name: string;
+    startdate:string
+};
+
+type Manager = {
+    name:string;
+    department:string
+}
+
+type TeamLead = Employee & Manager
+
+const teamLead:TeamLead = {
+    name: "Razz",
+    startdate: "23-11-2008",
+    department: "appD"
+}
+console.log(teamLead);
+
+
+let employee1:Employee = {
+    name:"Raju",
+    startdate: "12-08-2013"
+}
+
