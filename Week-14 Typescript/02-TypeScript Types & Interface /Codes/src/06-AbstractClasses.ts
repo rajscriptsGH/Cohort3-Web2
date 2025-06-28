97f76d410c56d3140d36fb7c2a6df045c5b9dedf
rajyadav63 <vector36razz@gmail.com>
2025-06-28 09:45:19 +0530

    abstract class

---
+++ Week-14 Typescript/02-TypeScript Types & Interface /Codes/src/06-AbstractClasses.ts
@@ -0,0 +1,30 @@
+ abstract class Student{
+    //property
+    name: string;
+    age: number;
+
+    //constructor for Student
+    constructor(name:string, age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+    //abstract method, must be implemented in subclass
+    abstract study(): void;
+
+    //default methiod
+    run(): void{
+        console.log(`${this.name} has gone for running`);
+    }
+}
+
+ class std1 extends Student{
+    study(): void {
+        console.log(`${this.name} is studying rn`);
+    }
+ }
+
+ const braj = new std1("Razz", 21);
+braj.run()
+braj.study()
+ 
