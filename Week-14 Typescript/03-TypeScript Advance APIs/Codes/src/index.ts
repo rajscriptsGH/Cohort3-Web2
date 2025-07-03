3334846aa6e3aa573869534d5a7a4d544997df4e
rajyadav63 <vector36razz@gmail.com>
2025-07-03 11:34:13 +0530

    API in ts

--- Week-14 Typescript/03-TypeScript Advance APIs/Codes/src/index.ts
+++ Week-14 Typescript/03-TypeScript Advance APIs/Codes/src/index.ts
@@ -7,10 +7,11 @@ function sumOfAge(user1: User, user2: User) {
     return user1.age + user2.age
 }
 
-let ages = sumOfAge({
-    name: "Razz",
-    age: 21
-},
+let ages = sumOfAge(
+    {
+        name: "Razz",
+        age: 21
+    },
     {
         name: "Raju",
         age: 25
