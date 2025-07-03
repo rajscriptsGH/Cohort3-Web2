334e7fbb649bec80f48692f391f13ea8dad8e6f2
rajyadav63 <vector36razz@gmail.com>
2025-07-03 09:11:12 +0530

    prereq for ts API

--- Week-14 Typescript/03-TypeScript Advance APIs/Codes/src/index.ts
+++ Week-14 Typescript/03-TypeScript Advance APIs/Codes/src/index.ts
@@ -1,5 +1,20 @@
-function age(a: number) {
-    console.log(a);
+interface User {
+    name: string;
+    age: number
 }
 
-age(2)
+function sumOfAge(user1: User, user2: User) {
+    return user1.age + user2.age
+}
+
+let ages = sumOfAge({
+    name: "Razz",
+    age: 21
+},
+    {
+        name: "Raju",
+        age: 25
+    }
+)
+
+console.log(ages);
