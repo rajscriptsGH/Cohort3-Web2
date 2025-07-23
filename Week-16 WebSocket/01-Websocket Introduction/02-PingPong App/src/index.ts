fb5a742748d457f76f506694939308ab1cdf6c92
rajyadav63 <vector36razz@gmail.com>
2025-07-23 10:33:58 +0530

    echo type ping pong

--- Week-16 WebSocket/01-Websocket Introduction/02-PingPong App/src/index.ts
+++ Week-16 WebSocket/01-Websocket Introduction/02-PingPong App/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hii");
+
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+//event handler
+wss.on('connection', (socket) => {
+    console.log('WS server connected');
+
+    //client 
+    socket.on("message", (e) => {
+        console.log(e.toString());
+        console.log(e.toString() === "ping");
+
+        if (e.toString() === 'ping')
+            socket.send("pong")
+    })
+})
