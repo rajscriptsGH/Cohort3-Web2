9100a6ba29c465ad9b3ac1e38e678d5452c00481
rajyadav63 <vector36razz@gmail.com>
2025-07-23 09:49:28 +0530

    client socket communication

--- Week-16 WebSocket/01-Websocket Introduction/01-Basics/src/index.ts
+++ Week-16 WebSocket/01-Websocket Introduction/01-Basics/src/index.ts
@@ -3,6 +3,17 @@ import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', () => {
-    console.log('WS server');
+//event handler
+wss.on('connection', (socket) => {
+    console.log('WS server connected');
+
+    setInterval(() => {
+        socket.send("Hello from WS " + Math.random())
+    }, 500)
+
+    //client 
+    socket.on("msg", (e) => {
+        console.log(e.toString());
+
+    })
 })
