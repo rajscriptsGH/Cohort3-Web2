b6812fa0139fbd53f274dc55e49485ee93565b18
rajyadav63 <vector36razz@gmail.com>
2025-07-23 12:13:45 +0530

    simple ping pong in chatapp

--- Week-16 WebSocket/01-Websocket Introduction/03-ChatApp (pp)/fe/src/App.tsx
+++ Week-16 WebSocket/01-Websocket Introduction/03-ChatApp (pp)/fe/src/App.tsx
@@ -1,12 +1,42 @@
 
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [socket, setSocket] = useState();
+  const inputRef = useRef(null)
+
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+
+    const msg = inputRef.current?.value;
+    //@ts-ignore
+    socket.send(msg)
+
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080")
+    setSocket(ws);
+
+
+
+    ws.onmessage = (ev) => {
+      alert(ev.data)
+    }
+  }, [])
 
   return (
-    <>
+    <div>
       <h1>hello Chat App</h1>
-    </>
+
+      <div className='h-screen flex justify-center items-center gap-2'>
+        <input ref={inputRef} className='h-9  rounded-lg px-4 py-2' type="text" placeholder='Message' />
+        <button className='bg-blue-500 px-4 py-2 rounded-lg cursor-pointer' onClick={sendMessage}>Send</button>
+      </div>
+    </div>
   )
 }
 
