445eb81e1fc612d24de29fb7615fd6c1e63bad07
rajyadav63 <vector36razz@gmail.com>
2025-07-25 09:53:27 +0530

    msg type join

--- Week-16 WebSocket/02-Chat App/backend/src/index.ts
+++ Week-16 WebSocket/02-Chat App/backend/src/index.ts
@@ -1,17 +1,34 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 })
-let userCount = 0;
+
+interface User {
+    socket: WebSocket;
+    room: string
+}
+
+let allSockets: User[] = []
+
 
 wss.on("connection", (socket) => {
-    userCount = userCount + 1
-    console.log(`UserCount # ${userCount}`);
+    console.log("Client connected");
+
 
     socket.on('message', (message) => {
-        console.log(`Message ${message.toString()} recieved form user ${userCount}`);
-        setTimeout(() => {
-            socket.send(`${message} send back from server`)
-        }, 2000)
+        const msg = JSON.parse(message.toString())
+        if (msg.type === 'join') {
+            allSockets.push({
+                socket,
+                room: msg.payload.roomId
+            })
+            console.log(`Client joind room: ${msg.roomId}`);
+
+        }
+    })
+
+    socket.on("close", () => {
+        allSockets = allSockets.filter((user) => user.socket !== socket);
+        console.log("Client disconnected");
 
     })
 })
