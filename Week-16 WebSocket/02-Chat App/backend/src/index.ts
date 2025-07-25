e5192ab2997828d0aab687f6c2619d9776666fc4
rajyadav63 <vector36razz@gmail.com>
2025-07-25 10:19:12 +0530

    chat type added

--- Week-16 WebSocket/02-Chat App/backend/src/index.ts
+++ Week-16 WebSocket/02-Chat App/backend/src/index.ts
@@ -16,13 +16,48 @@ wss.on("connection", (socket) => {
 
     socket.on('message', (message) => {
         const msg = JSON.parse(message.toString())
+
+        // User wants to join a room
         if (msg.type === 'join') {
             allSockets.push({
                 socket,
                 room: msg.payload.roomId
             })
-            console.log(`Client joind room: ${msg.roomId}`);
+            console.log(`Client joind room: ${msg.payload.roomId}`);
+        }
 
+        // User wants to chat a room
+        // Check Message Type
+        if (msg.type === 'chat') {
+
+            //Fins current user in allSocket
+            const user = allSockets.find((user) => user.socket === socket)
+
+            //Check if user exist or not
+            if (!user) {
+                console.log("User not found in room");
+                return;
+            }
+
+            //If user exist then extract room and text form user
+            const room = user.room
+            const text = msg.payload.text
+
+            //Broadcast to all user in that room
+            allSockets.forEach((user) => {
+                //Check for same room and sender themselves
+                if (user.room == room && user.socket !== socket) {
+
+                    //Send the chat messages
+                    user.socket.send(JSON.stringify({
+                        type: 'chat',
+                        payload: {
+                            text,
+                            room
+                        }
+                    }))
+                }
+            })
         }
     })
 
