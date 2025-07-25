2a0d892984d9be968d978231aa31b34ca698fa13
rajyadav63 <vector36razz@gmail.com>
2025-07-25 11:40:50 +0530

    chat box, input box, send button

--- Week-16 WebSocket/02-Chat App/frontend/src/App.tsx
+++ Week-16 WebSocket/02-Chat App/frontend/src/App.tsx
@@ -4,8 +4,16 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      Hii Chat
+    <div className='h-screen bg-black flex justify-center flex-col p-2'>
+      <h1 className='fixed top-0 left-48 bg-orange-500 h-10 rounded-lg text-white px-5 py-2 text-center font-semibold'>Chat App</h1>
+
+      <div className='h-[80vh] bg-slate-700 rounded-xl p-5 mb-2'>
+
+      </div>
+      <div className='flex justify-center gap-3'>
+        <input className='w-full bg-purple-800 text-white px-3 py-2 rounded-lg' type="text" placeholder='Write message' />
+        <button className='bg-blue-500 text-white px-7 py-2 rounded-lg'>Send</button>
+      </div>
     </div>
   )
 }
