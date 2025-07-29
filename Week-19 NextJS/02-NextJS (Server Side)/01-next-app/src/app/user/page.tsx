bd87fa7e20d61956e7f9fd41376f564a689b94da
rajyadav63 <vector36razz@gmail.com>
2025-07-29 10:06:00 +0530

    loading tsx

--- Week-19 NextJS/02-NextJS (Server Side)/01-next-app/src/app/user/page.tsx
+++ Week-19 NextJS/02-NextJS (Server Side)/01-next-app/src/app/user/page.tsx
@@ -1,14 +1,14 @@
+'use client'
 import React from 'react'
 import axios from 'axios'
-import { log } from 'console'
 
 //SSR => next js do server side rendering
 export default async function User() {
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1")
 
-    const data = response.data
+    await new Promise(r => setTimeout(r, 3000))
 
-    console.log("Hi");
+    const data = response.data
 
     return <div>
         {data.title}
