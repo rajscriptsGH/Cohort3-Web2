fe421c3eb16ff90c541c7c14c517da31f1d02bba
rajyadav63 <vector36razz@gmail.com>
2025-07-29 12:03:35 +0530

    prisma client in route

--- Week-19 NextJS/02-NextJS (Server Side)/03-fullstack-next-app/src/app/api/v1/signup/route.ts
+++ Week-19 NextJS/02-NextJS (Server Side)/03-fullstack-next-app/src/app/api/v1/signup/route.ts
@@ -1,11 +1,18 @@
 'use client'
 import { NextRequest, NextResponse } from "next/server";
-
+import { PrismaClient } from "@prisma/client/extension";
 
 export async function POST(req: NextRequest) {
 
     const data = await req.json()
     console.log(data);
+
+    await PrismaClient.user.create({
+        data: {
+            username: data.username,
+            password: data.password
+        }
+    })
 
 
     return NextResponse.json({
