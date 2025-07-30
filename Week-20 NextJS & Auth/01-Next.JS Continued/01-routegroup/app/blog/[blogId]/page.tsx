85a3693c4f9edaacef3113310eb52343bb4ee682
rajyadav63 <vector36razz@gmail.com>
2025-07-30 11:20:36 +0530

    param awaited

--- Week-20 NextJS & Auth/01-Next.JS Continued/01-routegroup/app/blog/[blogId]/page.tsx
+++ Week-20 NextJS & Auth/01-Next.JS Continued/01-routegroup/app/blog/[blogId]/page.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default async function ({ params }: { params: { blogId?: string } }) {
-    const postId = params.blogId;
+    const postId = (await params).blogId;
     const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${postId}`)
 
     const data = response.data
