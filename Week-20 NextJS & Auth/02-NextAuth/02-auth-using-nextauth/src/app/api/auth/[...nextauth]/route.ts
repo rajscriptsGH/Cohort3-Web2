a6968dedf1a7e90fe78532160f7c11f3fb1189d1
rajyadav63 <vector36razz@gmail.com>
2025-08-15 22:38:56 +0530

    providers in next auth

--- Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/api/auth/[...nextauth]/route.ts
+++ Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,25 @@
+import { useSession, signIn, signOut } from "next-auth/react";
 import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
 
 const handler = NextAuth({
     providers: [
-        
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
     ]
 })
 
 export { handler as GET, handler as POST }
+
+session: { strategy: "jwt" }
+
+callbacks: {
+    async jwt({ token, account }) {
+        if (account) {
+            token.accessToken = account.access_token
+        }
+        return token
+    }
+}
