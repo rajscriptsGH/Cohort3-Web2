9838cfba0e20bd6afbc366bf6d181ac0fa5b387f
rajyadav63 <vector36razz@gmail.com>
2025-08-16 11:53:52 +0530

    sign in and logout

--- Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/page.tsx
+++ Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/page.tsx
@@ -1,9 +1,37 @@
-import Image from "next/image";
+"use client"
+
+// import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+
+// export default function Home() {
+//   return (
+//     <SessionProvider>
+//       <RealHome />
+//     </SessionProvider>
+//   );
+// }
+
+
+// function RealHome() {
+//   const session = useSession()
+
+//   return (
+//     <div className="min-h-screen flex justify-center items-center">
+//       {session.status == "authenticated" &&
+//         <button className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
+//           onClick={() => signOut()}>Logout</button>}
+//       {session.status == "unauthenticated" &&
+//         <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+//           onClick={() => signIn()}>signin</button>}
+//     </div>
+//   )
+// }
 
 export default function Home() {
   return (
     <div>
-      Main page
+      Hii main
     </div>
   );
 }
+
+
