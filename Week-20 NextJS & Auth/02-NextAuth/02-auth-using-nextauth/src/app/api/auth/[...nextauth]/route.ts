b613fa7e2318bfc5a794d650c520a294a609d02f
rajyadav63 <vector36razz@gmail.com>
2025-08-16 10:13:42 +0530

    sign with email

--- Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/api/auth/[...nextauth]/route.ts
+++ Week-20 NextJS & Auth/02-NextAuth/02-auth-using-nextauth/src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,31 @@
-import { useSession, signIn, signOut } from "next-auth/react";
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
+        CredentialsProvider({
+            name: "Login with Email",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "Razz" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                const user = {
+                    id: "1",
+                    name: "RK Razz",
+                    email: "razz@example.com"
+                }
+
+                if (user) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        })
     ]
 })
 
 export { handler as GET, handler as POST }
 
-session: { strategy: "jwt" }
 
-callbacks: {
-    async jwt({ token, account }) {
-        if (account) {
-            token.accessToken = account.access_token
-        }
-        return token
-    }
-}
