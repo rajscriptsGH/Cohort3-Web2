aa6c971f608a9de0a2de3e7765663895e8812c91
rajyadav63 <vector36razz@gmail.com>
2025-08-19 12:15:42 +0530

    docs build

--- Week-21 MonoRepo & TurboRepo/01-Mono & Turbo Introduction/apps/docs/app/page.tsx
+++ Week-21 MonoRepo & TurboRepo/01-Mono & Turbo Introduction/apps/docs/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy right now
           </a>
           <a
             href="https://turborepo.com/docs?utm_source"
