5959ae3961e44015401cee1d2c621bbf25c267b9
rajyadav63 <vector36razz@gmail.com>
2025-08-25 11:59:20 +0530

    node working with pg

--- Week-17 Postgres & SQL db/SQL-I/01-ToDoApp/src/index.ts
+++ Week-17 Postgres & SQL db/SQL-I/01-ToDoApp/src/index.ts
@@ -1,16 +1,21 @@
 import { Client } from 'pg'
 
-const pgClient = new Client({
-    user: "neondb_owner",
-    password: "npg_hZedaC6T2Vlc",
-    port: 5431,
-    host: "ep-empty-tooth-adeuvoex-pooler.c-2.us-east-1.aws.neon.tech",
-    database: "neondb"
-})
+const pgClient = new Client("postgresql://neondb_owner:npg_hZedaC6T2Vlc@ep-empty-tooth-adeuvoex-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+
+// const pgClient = new Client({
+//     user: "neondb_owner",
+//     password: "npg_hZedaC6T2Vlc",
+//     port: 5431,
+//     host: "ep-empty-tooth-adeuvoex-pooler.c-2.us-east-1.aws.neon.tech",
+//     database: "neondb",
+//     ssl: true
+// })
 
 async function dbConnect() {
     await pgClient.connect()
     const response = await pgClient.query("SELECT * FROM users")
-    console.log(response);
+    console.log(response.rows);
 
 }
+
+dbConnect()
